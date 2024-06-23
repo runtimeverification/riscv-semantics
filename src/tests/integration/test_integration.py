f6a62b4bf7c291f4334f3114e2e6f48d0fe8a1e3
Scott Guest <scott.guest@runtimeverification.com>
2024-06-23 17:33:29 -0400

    Stub out simple integration tests

--- src/tests/integration/test_integration.py
+++ src/tests/integration/test_integration.py
@@ -1,5 +1,30 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+
 from kriscv.hello import hello
+
+from ..utils import TESTS_DIR
+
+if TYPE_CHECKING:
+    from pathlib import Path
+    from typing import Final
 
 
 def test_hello() -> None:
     assert hello('World') == 'Hello, World!'
+
+
+SIMPLE_DIR: Final = TESTS_DIR / 'simple'
+SIMPLE_TESTS: Final = tuple(asm_file for asm_file in SIMPLE_DIR.rglob('*.S'))
+
+
+@pytest.mark.parametrize(
+    'asm_file',
+    SIMPLE_TESTS,
+    ids=[str(test.relative_to(SIMPLE_DIR)) for test in SIMPLE_TESTS],
+)
+def test_simple(asm_file: Path) -> None:
+    print(asm_file)
