37016d634aa5e2623c56b984ef5b7e95896331d7
Scott Guest <scott.guest@runtimeverification.com>
2024-07-01 00:53:12 -0400

    Add --save-temps pytest option and temp_dir fixture shared among all xdist workers

--- src/tests/integration/test_integration.py
+++ src/tests/integration/test_integration.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import subprocess
 from pathlib import Path
-from tempfile import TemporaryDirectory
 from typing import TYPE_CHECKING
 
 import pytest
@@ -35,25 +34,24 @@ def _test_simple(elf_file: Path, expected_file: Path, update: bool) -> None:
     SIMPLE_TESTS,
     ids=[str(test.relative_to(SIMPLE_DIR)) for test in SIMPLE_TESTS],
 )
-def test_simple(asm_file: Path, update_expected_output: bool) -> None:
-    with TemporaryDirectory() as temp_dir:
-        elf_file = Path(temp_dir) / (asm_file.stem + '.elf')
-        compile_cmd = [
-            'riscv64-unknown-elf-gcc',
-            '-nostdlib',
-            '-nostartfiles',
-            '-static',
-            '-march=rv32e',
-            '-mabi=ilp32e',
-            '-mno-relax',
-            '-mlittle-endian',
-            '-Xassembler',
-            '-mno-arch-attr',
-            str(asm_file),
-            '-o',
-            str(elf_file),
-        ]
-        subprocess.run(compile_cmd, check=True)
-        assert elf_file.exists()
-        expected_file = Path(str(asm_file) + '.out').resolve(strict=True)
-        _test_simple(elf_file, expected_file, update_expected_output)
+def test_simple(asm_file: Path, update_expected_output: bool, temp_dir: Path) -> None:
+    elf_file = Path(temp_dir) / (asm_file.stem + '.elf')
+    compile_cmd = [
+        'riscv64-unknown-elf-gcc',
+        '-nostdlib',
+        '-nostartfiles',
+        '-static',
+        '-march=rv32e',
+        '-mabi=ilp32e',
+        '-mno-relax',
+        '-mlittle-endian',
+        '-Xassembler',
+        '-mno-arch-attr',
+        str(asm_file),
+        '-o',
+        str(elf_file),
+    ]
+    subprocess.run(compile_cmd, check=True)
+    assert elf_file.exists()
+    expected_file = Path(str(asm_file) + '.out').resolve(strict=True)
+    _test_simple(elf_file, expected_file, update_expected_output)
