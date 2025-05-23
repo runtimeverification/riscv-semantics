086ae735554fb50f5828fcdba34c0c3fe2f070a7
Tamás Tóth <tothtamas28@users.noreply.github.com>
2025-05-23 14:03:27 +0000

    Remove function `sparse_bytes`

--- src/kriscv/term_builder.py
+++ src/kriscv/term_builder.py
@@ -1,17 +1,13 @@
 from __future__ import annotations
 
-import itertools
-from typing import TYPE_CHECKING, cast
+from typing import TYPE_CHECKING
 
 from pyk.kast.inner import KApply, KInner, KSort, KVariable
-from pyk.kast.prelude.bytes import bytesToken
 from pyk.kast.prelude.collections import map_of
 from pyk.kast.prelude.kint import intToken
 
-from kriscv.term_manip import normalize_memory
-
 if TYPE_CHECKING:
-    from collections.abc import Iterable, Mapping
+    from collections.abc import Iterable
     from typing import TypeVar
 
     T = TypeVar('T')
@@ -275,39 +271,6 @@ def length_bytes(k: KInner | str) -> KInner:
 
 def add_bytes(bytes1: KInner, bytes2: KInner) -> KInner:
     return KApply('_+Bytes__BYTES-HOOKED_Bytes_Bytes_Bytes', bytes1, bytes2)
-
-
-def sparse_bytes(data: Mapping[int, bytes]) -> KInner:
-    clean_data: list[tuple[int, bytes]] = sorted(normalize_memory(data).items())
-
-    if len(clean_data) == 0:
-        return dot_sb()
-
-    # Collect all empty gaps between segements
-    gaps = []
-    start = clean_data[0][0]
-    if start != 0:
-        gaps.append((0, start))
-    for (start1, val1), (start2, _) in itertools.pairwise(clean_data):
-        end1 = start1 + len(val1)
-        # normalize_memory should already have merged consecutive segments
-        assert end1 < start2
-        gaps.append((end1, start2 - end1))
-
-    # Merge segments and gaps into a list of sparse bytes items
-    sparse_data: list[tuple[int, int | bytes]] = sorted(
-        cast('list[tuple[int, int | bytes]]', clean_data) + cast('list[tuple[int, int | bytes]]', gaps), reverse=True
-    )
-
-    sparse_k = dot_sb()
-    for _, gap_or_val in sparse_data:
-        if isinstance(gap_or_val, int):
-            sparse_k = sb_empty_cons(sb_empty(intToken(gap_or_val)), sparse_k)
-        elif isinstance(gap_or_val, bytes):
-            sparse_k = sb_bytes_cons(sb_bytes(bytesToken(gap_or_val)), sparse_k)
-        else:
-            raise AssertionError()
-    return sparse_k
 
 
 def sort_memory() -> KSort:
