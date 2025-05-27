329a0b9f1a676fcd2c7758ea28b213eb5cafb456
Tamás Tóth <tothtamas28@users.noreply.github.com>
2025-05-27 12:08:55 +0000

    Write temporary test data into separate folders by default

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,15 +1,12 @@
 from __future__ import annotations
 
-import shutil
-import tempfile
 from pathlib import Path
 from typing import TYPE_CHECKING
 
 import pytest
 
 if TYPE_CHECKING:
-    from pytest import Config, FixtureRequest, Parser
-    from xdist import WorkerController  # type: ignore
+    from pytest import FixtureRequest, Parser
 
 
 def pytest_addoption(parser: Parser) -> None:
@@ -32,31 +29,8 @@ def save_final_config(request: FixtureRequest) -> bool:
 
 
 @pytest.fixture
-def temp_dir(request: FixtureRequest) -> Path:
-    if is_master(request.config):
-        return Path(request.config.temp_dir)  # type: ignore[attr-defined]
-    else:
-        return Path(request.config.workerinput['temp_dir'])  # type: ignore[attr-defined]
-
-
-def pytest_configure(config: Config) -> None:
-    if is_master(config):
-        temp_dir = config.getoption('--temp-dir')
-        if temp_dir is None:
-            temp_dir = Path(tempfile.mkdtemp())
-        else:
-            temp_dir.mkdir(parents=True, exist_ok=True)
-        config.temp_dir = str(temp_dir.resolve(strict=True))  # type: ignore[attr-defined]
-
-
-def pytest_configure_node(node: WorkerController) -> None:
-    node.workerinput['temp_dir'] = node.config.temp_dir
-
-
-def pytest_unconfigure(config: Config) -> None:
-    if is_master(config) and config.getoption('--temp-dir') is None:
-        shutil.rmtree(config.temp_dir)  # type: ignore[attr-defined]
-
-
-def is_master(config: Config) -> bool:
-    return not hasattr(config, 'workerinput')
+def temp_dir(request: FixtureRequest, tmp_path: Path) -> Path:
+    temp_dir = request.config.getoption('--temp-dir')
+    if temp_dir is not None:
+        return temp_dir
+    return tmp_path
