742ca7df84d513343cf26121a14081a19ae3a988
Tamás Tóth <tothtamas28@users.noreply.github.com>
2025-05-30 13:28:18 +0000

    Migrate from `poetry` to `uv`

--- flake.nix
+++ flake.nix
@@ -1,41 +1,74 @@
 {
   description = "kriscv - K tooling for the RISC-V architecture";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.05";
+    nixpkgs.url = "nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
-    poetry2nix.url = "github:nix-community/poetry2nix";
+    uv2nix.url = "github:pyproject-nix/uv2nix/680e2f8e637bc79b84268949d2f2b2f5e5f1d81c";
+    # stale nixpkgs is missing the alias `lib.match` -> `builtins.match`
+    # therefore point uv2nix to a patched nixpkgs, which introduces this alias
+    # this is a temporary solution until nixpkgs us up-to-date again
+    uv2nix.inputs.nixpkgs.url = "github:runtimeverification/nixpkgs/libmatch";
+    # inputs.nixpkgs.follows = "nixpkgs";
+    pyproject-build-systems.url = "github:pyproject-nix/build-system-pkgs/7dba6dbc73120e15b558754c26024f6c93015dd7";
+    pyproject-build-systems = {
+      inputs.nixpkgs.follows = "uv2nix/nixpkgs";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.pyproject-nix.follows = "uv2nix/pyproject-nix";
+    };
+    pyproject-nix.follows = "uv2nix/pyproject-nix";
   };
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    let
-      allOverlays = [
-        poetry2nix.overlay
-        (final: prev: {
-          kriscv = prev.poetry2nix.mkPoetryApplication {
-            python = prev.python310;
-            projectDir = ./.;
-            groups = [];
-            # We remove `dev` from `checkGroups`, so that poetry2nix does not try to resolve dev dependencies.
-            checkGroups = [];
-           };
-        })
-      ];
-    in flake-utils.lib.eachSystem [
+  outputs = { self, nixpkgs, flake-utils, pyproject-nix, pyproject-build-systems, uv2nix }:
+  let
+    pythonVer = "310";
+  in flake-utils.lib.eachSystem [
       "x86_64-linux"
       "x86_64-darwin"
       "aarch64-linux"
       "aarch64-darwin"
     ] (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = allOverlays;
-        };
-      in {
-        packages = rec {
-          inherit (pkgs) kriscv;
-          default = kriscv;
-        };
-      }) // {
-        overlay = nixpkgs.lib.composeManyExtensions allOverlays;
+    let
+      # due to the nixpkgs that we use in this flake being outdated, uv is also heavily outdated
+      # we can instead use the binary release of uv provided by uv2nix for now
+      uvOverlay = final: prev: {
+        uv = uv2nix.packages.${final.system}.uv-bin;
       };
+      kriscvOverlay = final: prev: {
+        kriscv = final.callPackage ./nix/kriscv {
+          inherit pyproject-nix pyproject-build-systems uv2nix;
+          python = final."python${pythonVer}";
+        };
+      };
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          uvOverlay
+          kriscvOverlay
+        ];
+      };
+      python = pkgs."python${pythonVer}";
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "uv develop shell";
+        buildInputs = [
+          python
+          pkgs.uv
+        ];
+        env = {
+          # prevent uv from managing Python downloads and force use of specific 
+          UV_PYTHON_DOWNLOADS = "never";
+          UV_PYTHON = python.interpreter;
+        };
+        shellHook = ''
+          unset PYTHONPATH
+        '';
+      };
+      packages = rec {
+        inherit (pkgs) kriscv;
+        default = kriscv;
+      };
+    }) // {
+      overlays.default = final: prev: {
+        inherit (self.packages.${final.system}) kriscv;
+      };
+    };
 }
